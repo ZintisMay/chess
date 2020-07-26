906d7f8206420d79695867ecbf5454b67882ba04
ZintisMay <zbmayk@gmail.com>
2020-07-26 11:30:31 -0400

    2nd

--- app.js
+++ app.js
@@ -3,24 +3,16 @@ const log = console.log
 class Chess {
     constructor(target, size) {
         this.target = target
+        this.white = []
+        this.black = []
 
-        this.black = this.createPieces("black")
-        this.white = this.createPieces("white")
+        this.createPieces()
         this.createBoard()
         this.setBoard()
         this.drawBoard()
         log(this)
     }
-    drawBoard() {
-        var switcher = false
-        this.board.map((row, x) => {
-            switcher = !switcher
-            row.map((cell, y) => {
-                switcher = !switcher
-                this.target.innerHTML += this.createCell(cell, x, y, switcher ? "dark" : "")
-            })
-        })
-    }
+
     createBoard() {
         this.board = []
         //Create board array
@@ -33,13 +25,54 @@ class Chess {
         }
         log(this.board)
     }
-    createCell(cell, x, y, color) {
-        return `<div class="cell ${x} ${y} ${color}">${cell.name || "" }</div>`
+    setBoard() {
+    		log(this.black)
+    		log(this.white)
+    		log("mapping black")
+    		var x = 0;
+        this.black.map(piece => {
+        		x++
+            log(x,piece)
+            log(this.board)
+            this.board[piece.x][piece.y] = piece
+        })
+    		log("mapping white")
+        this.white.map(piece => {
+        		x++
+            log(x,piece)
+            log(this.board)
+            this.board[piece.x][piece.y] = piece
+            // this.board[piece.x][piece.y] = piece
+        })
     }
-    createPieces(color) {
+    drawBoard() {
+        var switcher = false
+        this.board.map((row, x) => {
+            switcher = !switcher
+            row.map((cell, y) => {
+                switcher = !switcher
+                this.target.appendChild(this.createCell(cell, x, y, switcher ? "dark" : ""))  
+            })
+        })
+    }
+    createCell(cell, x, y, color) {
+    		var div = document.createElement("div")
+    		div.classList.add("cell")
+    		div.classList.add(x)
+    		div.classList.add(y)
+    		color ? div.classList.add(color) : null;
+    		
+
+    		div.innerHTML = `${cell?.name || "" }`
+    		div.addEventListener('click', function(e){
+    			alert("bob")
+    		})
+        return div 
+    }
+    createPieces() {
 
         var p = []
-        
+
         var pawnStats = { name: "pawn", direction: 1, move: [{ x: 0, y: 1 }], attack: [{ x: 1, y: 1 }, { x: -1, y: 1 }] }
         var rookStats = { name: "rook", attack: [{ x: 1, y: 0, infinite: true }, { x: -1, y: 0, infinite: true }, { x: 0, y: 1, infinite: true }, { x: 0, y: -1, infinite: true }] }
         var knightStats = { name: "knight", attack: [{ x: 2, y: 1 }, { x: -2, y: 1 }, { x: 2, y: -1 }, { x: -2, y: -1 }, { x: 1, y: 2 }, { x: 1, y: -2 }, { x: -1, y: 2 }, { x: -1, y: -2 }] }
@@ -47,55 +80,47 @@ class Chess {
         var queenStats = { name: "queen", attack: [{ x: 1, y: 1, infinite: true }, { x: -1, y: 1, infinite: true }, { x: 1, y: -1, infinite: true }, { x: -1, y: -1, infinite: true }, { x: 1, y: 0, infinite: true }, { x: -1, y: 0, infinite: true }, { x: 0, y: 1, infinite: true }, { x: 0, y: -1, infinite: true }] }
         var kingStats = { name: "king", attack: [{ x: 1, y: 1 }, { x: -1, y: 1 }, { x: 1, y: -1 }, { x: -1, y: -1 }] }
 
-        p.push(new ChessPiece(pawnStats, "white", 0, 1))
-        p.push(new ChessPiece(pawnStats, "white", 1, 1))
-        p.push(new ChessPiece(pawnStats, "white", 2, 1))
-        p.push(new ChessPiece(pawnStats, "white", 3, 1))
-        p.push(new ChessPiece(pawnStats, "white", 4, 1))
-        p.push(new ChessPiece(pawnStats, "white", 5, 1))
-        p.push(new ChessPiece(pawnStats, "white", 6, 1))
-        p.push(new ChessPiece(pawnStats, "white", 7, 1))
-        p.push(new ChessPiece(rookStats, "white"), 0, 0)
-        p.push(new ChessPiece(rookStats, "white"), 7, 0)
-        p.push(new ChessPiece(knightStats, "white"), 1, 0)
-        p.push(new ChessPiece(knightStats, "white"), 6, 0)
-        p.push(new ChessPiece(bishopStats, "white"), 2, 0)
-        p.push(new ChessPiece(bishopStats, "white"), 5, 0)
-        p.push(new ChessPiece(queenStats, "white"), 3, 0)
-        p.push(new ChessPiece(kingStats, "white"), 4, 0)
+        this.white.push(new ChessPiece(pawnStats, "white", 1, 0))
+        this.white.push(new ChessPiece(pawnStats, "white", 1, 1))
+        this.white.push(new ChessPiece(pawnStats, "white", 1, 2))
+        this.white.push(new ChessPiece(pawnStats, "white", 1, 3))
+        this.white.push(new ChessPiece(pawnStats, "white", 1, 4))
+        this.white.push(new ChessPiece(pawnStats, "white", 1, 5))
+        this.white.push(new ChessPiece(pawnStats, "white", 1, 6))
+        this.white.push(new ChessPiece(pawnStats, "white", 1, 7))
+        this.white.push(new ChessPiece(rookStats, "white", 0, 0))
+        this.white.push(new ChessPiece(rookStats, "white", 0, 7))
+        this.white.push(new ChessPiece(knightStats, "white", 0, 1))
+        this.white.push(new ChessPiece(knightStats, "white", 0, 6))
+        this.white.push(new ChessPiece(bishopStats, "white", 0, 2))
+        this.white.push(new ChessPiece(bishopStats, "white", 0, 5))
+        this.white.push(new ChessPiece(queenStats, "white", 0, 3))
+        this.white.push(new ChessPiece(kingStats, "white", 0, 4))
 
-        p.push(new ChessPiece(pawnStats, "black", 0, 6))
-        p.push(new ChessPiece(pawnStats, "black", 1, 6))
-        p.push(new ChessPiece(pawnStats, "black", 2, 6))
-        p.push(new ChessPiece(pawnStats, "black", 3, 6))
-        p.push(new ChessPiece(pawnStats, "black", 4, 6))
-        p.push(new ChessPiece(pawnStats, "black", 5, 6))
-        p.push(new ChessPiece(pawnStats, "black", 6, 6))
-        p.push(new ChessPiece(pawnStats, "black", 7, 6))
-        p.push(new ChessPiece(rookStats, "black"), 0, 7)
-        p.push(new ChessPiece(rookStats, "black"), 7, 7)
-        p.push(new ChessPiece(knightStats, "black"), 1, 7)
-        p.push(new ChessPiece(knightStats, "black"), 6, 7)
-        p.push(new ChessPiece(bishopStats, "black"), 2, 7)
-        p.push(new ChessPiece(bishopStats, "black"), 5, 7)
-        p.push(new ChessPiece(queenStats, "black"), 3, 7)
-        p.push(new ChessPiece(kingStats, "black"), 4, 7)
+        this.black.push(new ChessPiece(pawnStats, "black", 6, 0))
+        this.black.push(new ChessPiece(pawnStats, "black", 6, 1))
+        this.black.push(new ChessPiece(pawnStats, "black", 6, 2))
+        this.black.push(new ChessPiece(pawnStats, "black", 6, 3))
+        this.black.push(new ChessPiece(pawnStats, "black", 6, 4))
+        this.black.push(new ChessPiece(pawnStats, "black", 6, 5))
+        this.black.push(new ChessPiece(pawnStats, "black", 6, 6))
+        this.black.push(new ChessPiece(pawnStats, "black", 6, 7))
+        this.black.push(new ChessPiece(rookStats, "black", 7, 0))
+        this.black.push(new ChessPiece(rookStats, "black", 7, 7))
+        this.black.push(new ChessPiece(knightStats, "black", 7, 1))
+        this.black.push(new ChessPiece(knightStats, "black", 7, 6))
+        this.black.push(new ChessPiece(bishopStats, "black", 7, 2))
+        this.black.push(new ChessPiece(bishopStats, "black", 7, 5))
+        this.black.push(new ChessPiece(queenStats, "black", 7, 3))
+        this.black.push(new ChessPiece(kingStats, "black", 7, 4))
 
-        return p
     }
-    setBoard() {
-        this.black.map(piece => {
-            log(piece)
-            this.board[piece.x][piece.y] = piece
-        })
-        this.white.map(piece => {
-            this.board[piece.x][piece.y] = piece
-        })
-    }
+
 }
 
 class ChessPiece {
     constructor(moves, color, x, y) {
+    		this.name = moves.name
         this.moves = moves
         this.move(x, y)
     }
@@ -104,36 +129,21 @@ class ChessPiece {
         this.y = y
     }
     getMoves(board) {
-        return this.moves.map(this.checkMove).filter(item => item)
+        return this.moves.map(this.checkMove, board).filter(item => item)
     }
-    checkMove(item) {
+    checkMove(item, board) {
 
     }
 }
 
-
-
-// var pawn = new ChessPiece(pawnStats)
-// var rook = new ChessPiece(rookStats)
-// var knight = new ChessPiece(knightStats)
-// var bishop = new ChessPiece(bishopStats)
-// var queen = new ChessPiece(queenStats)
-// var king = new ChessPiece(kingStats)
-
-// log(pawn)
-// log(rook)
-// log(knight)
-// log(bishop)
-// log(queen)
-// log(king)
 
 var target = document.getElementById('target')
 var game = new Chess(target)
 
-function repeat(times, item) {
-    var arr = []
-    for (var x = 0; x < times; x++) {
-        arr.push(item)
-    }
-    return arr
-}
+// function repeat(times, item) {
+//     var arr = []
+//     for (var x = 0; x < times; x++) {
+//         arr.push(item)
+//     }
+//     return arr
+// }
